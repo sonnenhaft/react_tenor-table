a9254868355264e287a22e23ae36ab3e54b2ac10
Vladimir Shein <vladimir.sonnenhaft@gmail.com>
2019-08-15 13:31:39 +0800

    added js and jsx support, fixed ts support

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,65 @@
-import React, { Component } from 'react'
-import { Widget } from './Widget'
+import React from 'react';
+import { Widget } from './Widget';
 
-// import { map } from 'lodash-es'
-// import { where } from 'ramda'
+import b from './sample-ts-import';
+import a from './sample-js-import';
+import { SampleJsxImport } from './SampleJsxImport';
 
-export const App = () => <div>
-    <p>
+let focus = function() {
+  console.log('activated');
+};
+let blur = function() {
+  console.log('deactivated');
+};
+
+// https://greensock.com/forums/topic/9059-cross-browser-to-detect-tab-or-window-is-active-so-animations-stay-in-sync-using-html5-visibility-api/
+function subscribeToDocumentInactive(fn: (isActive: boolean, documentHiddenEvent?: Event) => void) {
+// @ts-ignore
+  fn(document.visible, null);
+
+  const subscribeToDocumentHiddenEvent = function(documentHiddenEvent: Event) {
+    // @ts-ignore
+    fn.call(this, document.visible, documentHiddenEvent);
+  };
+
+  document.addEventListener('visibilitychange', subscribeToDocumentHiddenEvent);
+  return function unsubscribeFromDocumentHiddenEvent() {
+    document.removeEventListener('visibilitychange', subscribeToDocumentHiddenEvent);
+  };
+}
+
+export class App extends React.Component {
+  unsubscribeFromDocumentHiddenEvent?: () => void;
+
+  componentDidMount(): void {
+    console.log('componentDidMount');
+    a();
+    b();
+
+    window.addEventListener('focus', focus);
+    window.addEventListener('blur', blur);
+    this.unsubscribeFromDocumentHiddenEvent = subscribeToDocumentInactive((isActive: boolean) => {
+      console.log('isDocumentActive', isActive);
+    });
+
+  }
+
+  componentWillUnmount() {
+    console.log('componentWillUnmount');
+    window.removeEventListener('focus', focus);
+    window.removeEventListener('blur', blur);
+    this.unsubscribeFromDocumentHiddenEvent();
+  }
+
+  render() {
+    return <div>
+      <SampleJsxImport />
+      <p>
         <a href="/storybook">storybook</a>
-    </p>
+      </p>
 
-    TS configured
-    <Widget/>
-</div>
+      TS configured
+      <Widget />
+    </div>;
+  }
+}
