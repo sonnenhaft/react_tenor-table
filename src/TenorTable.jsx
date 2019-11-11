255fd314cf7f1a0d74789175bde5eeab3a674572
Vladimir Shein <vladimir.sonnenhaft@gmail.com>
2019-11-11 19:10:13 +0800

    added github pages

--- src/TenorTable.jsx
+++ src/TenorTable.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import style from 'styled-components';
 
 const Table = style.table`
@@ -31,21 +31,21 @@ const Table = style.table`
 `;
 
 const getData = (C, D, F, G) => {
-    const E = D * C * 5 / 1000;
-    const H = G + C / 10000;
-    const I = (H - G) * 10000;
-    const J = I * D;
-    const K = C * 25;
-    const L = K + J;
+  const E = D * C * 5 / 1000;
+  const H = G + C / 10000;
+  const I = (H - G) * 10000;
+  const J = I * D;
+  const K = C * 25;
+  const L = K + J;
 
-    return { E, H, I, J, K, L };
+  return { E, H, I, J, K, L };
 };
 
 
 function getValues() {
-    const map = { D: 1, W: 7, M: 30, Y: 365 };
+  const map = { D: 1, W: 7, M: 30, Y: 365 };
 
-    return `1D 1000
+  return `1D 1000
 1W 1200
 1M 1400
 3M 1600
@@ -56,94 +56,90 @@ function getValues() {
 10Y 1800
 20Y 2000
 20Y 2200`.split('\n').map((item, idx) => {
-        const [A, risk] = item.split(' ');
-        const [number, prefix] = A.replace(/([0-9]+)(.*)/g, '$1_$2').split('_');
-        const B = number * map[prefix];
-        const C = Math.round(Math.random() * 100) - 50;
-        const D = Number(risk);
-        const F = D * (1 + C * 5 / 1000);
-        const G = 0.05 + 0.003 * idx;
+    const [A, risk] = item.split(' ');
+    const [number, prefix] = A.replace(/([0-9]+)(.*)/g, '$1_$2').split('_');
+    const B = number * map[prefix];
+    const C = Math.round(Math.random() * 100) - 50;
+    const D = Number(risk);
+    const F = D * (1 + C * 5 / 1000);
+    const G = 0.05 + 0.003 * idx;
 
-        const { E, H, I, J, K, L } = getData(C, D, F, G);
-        return { A, B, C, D, E, F, G, H, I, J, K, L }
-    });
+    const { E, H, I, J, K, L } = getData(C, D, F, G);
+    return { A, B, C, D, E, F, G, H, I, J, K, L }
+  });
 }
 
 const splitDigits = n => n.toLocaleString();
 const addEconomicalNegativeBrackets = n => n > 0 ? splitDigits(n) :
-    <span style={{ color: 'red' }}>({splitDigits(-n)})</span>;
+  <span style={ { color: 'red' } }>({ splitDigits(-n) })</span>;
 const round2Digits = n => (Math.round(n * 100) / 100).toFixed(2);
 const showAsPercent = n => round2Digits(n * 100) + '%';
 const simpleAndBrackets = n => addEconomicalNegativeBrackets(Math.round(n));
 const tenorStyle = { background: 'black', color: 'white', fontWeight: 'bold', textAlign: 'center' };
 const meta = [
-    { key: 'A', label: 'Tenor', format: n => n, total: 'Total', style: tenorStyle },
-    { key: 'B', label: 'Sort label', format: n => n, total: ' ' },
-    { key: 'C', label: 'Random', format: n => n, total: ' ' },
-    { key: 'D', label: 'Risk', format: splitDigits },
-    { key: 'E', label: 'NB Risk', format: addEconomicalNegativeBrackets, style: { background: '#ffe598' } },
-    { key: 'F', label: 'Live Risk', format: addEconomicalNegativeBrackets, editable: true },
-    { key: 'G', label: 'Close(%)', format: showAsPercent, total: 'avg' },
-    { key: 'H', label: 'Live(%)', format: showAsPercent, total: 'avg' },
-    { key: 'I', label: 'Move(BP)', format: round2Digits, total: 'avg' },
-    { key: 'J', label: 'Live PL', format: simpleAndBrackets, style: { background: '#f7caac' } },
-    { key: 'K', label: 'NB PL', format: simpleAndBrackets, style: { background: '#f7caac' } },
-    { key: 'L', label: 'PL', format: simpleAndBrackets, style: { background: '#ffe598' } },
+  { key: 'A', label: 'Tenor', format: n => n, total: 'Total', style: tenorStyle },
+  { key: 'B', label: 'Sort label', format: n => n, total: ' ' },
+  { key: 'C', label: 'Random', format: n => n, total: ' ' },
+  { key: 'D', label: 'Risk', format: splitDigits },
+  { key: 'E', label: 'NB Risk', format: addEconomicalNegativeBrackets, style: { background: '#ffe598' } },
+  { key: 'F', label: 'Live Risk', format: addEconomicalNegativeBrackets, editable: true },
+  { key: 'G', label: 'Close(%)', format: showAsPercent, total: 'avg' },
+  { key: 'H', label: 'Live(%)', format: showAsPercent, total: 'avg' },
+  { key: 'I', label: 'Move(BP)', format: round2Digits, total: 'avg' },
+  { key: 'J', label: 'Live PL', format: simpleAndBrackets, style: { background: '#f7caac' } },
+  { key: 'K', label: 'NB PL', format: simpleAndBrackets, style: { background: '#f7caac' } },
+  { key: 'L', label: 'PL', format: simpleAndBrackets, style: { background: '#ffe598' } }
 ];
 
-export class TenorTable extends React.Component {
-    state = { values: getValues() };
+export const TenorTable = () => {
+  const [values, setValues] = useState(getValues);
 
-    componentWillMount() {
-        this.interval = setInterval(() => {
-            this.setState({ values: getValues() })
-        }, 5000);
-    }
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setValues(getValues())
+    }, 5000);
 
-    componentWillUnmount() {
-        clearInterval(this.interval);
-    }
+    return () => clearInterval(interval);
+  }, []);
 
-    editNumberFromContentEditable = item => (e) => {
-        const text = e.target.innerText;
+  const editNumberFromContentEditable = item => (e) => {
+    const text = e.target.innerText;
 
-        // may be we can catch some unnecessary values here,
-        // but in general better to parse user's data, rather than to show "error data"
-        const n = Number(text.replace(/[^0-9]+/g, ''));
+    // may be we can catch some unnecessary values here,
+    // but in general better to parse user's data, rather than to show "error data"
+    const n = Number(text.replace(/[^0-9]+/g, ''));
 
-        const F = isNaN(n) ? 0 : text.includes('(') ? -n : n;
-        const C = Math.round((F / item.D - 1) / 5 * 1000);
-        const { E, H, I, J, K, L } = getData(C, item.D, F, item.G);
+    const F = isNaN(n) ? 0 : text.includes('(') ? -n : n;
+    const C = Math.round((F / item.D - 1) / 5 * 1000);
+    const { E, H, I, J, K, L } = getData(C, item.D, F, item.G);
 
-        // in this size of sources it is fine to mutate here,
-        // in bigger up, most of time, would be better to extract
-        // this action somewhere
-        Object.assign(item, { C, E, H, I, J, K, L, tempTotalF: F });
-        this.forceUpdate();
-    };
+    // in this size of sources it is fine to mutate here,
+    // in bigger up, most of time, would be better to extract
+    // this action somewhere
+    Object.assign(item, { C, E, H, I, J, K, L, tempTotalF: F });
+    setValues([...values]);
+  };
 
-    render() {
-        return <Table>
-            <tr>{meta.map(({ key, label }) => <th key={key}>{label}</th>)}</tr>
+  return <Table>
+    <tr>{ meta.map(({ key, label }) => <th key={ key }>{ label }</th>) }</tr>
 
-            {this.state.values.map((item, index) => <tr key={index}>
-                {meta.map(({ format, key, style, editable }) => {
-                    // I agree that content editable here is not the best choice, but suddenly, during dev,
-                    // I liked how it looks and performs and how much code require
-                    return editable
-                        ? <td style={{ ...style, cursor: 'pencil', background: '#dadada' }} key={key} contentEditable
-                              onInput={this.editNumberFromContentEditable(item)}>
-                            {format(item[key])}
-                        </td>
-                        : <td style={style} key={key}>{format(item[key])}</td>
-                })}
-            </tr>)}
-            <tr>{meta.map(({ total, format, key }) => {
-                return <th key={key}>{total && total !== 'avg' ? total : format(this.state.values
-                    .reduce((sum, item) => {
-                        return sum + (item.hasOwnProperty('tempTotal' + key) ? item['tempTotal' + key] : item[key])
-                    }, 0) / (total === 'avg' ? this.state.values.length : 1))}</th>
-            })}</tr>
-        </Table>;
-    }
-}
+    { values.map((item, index) => <tr key={ index }>
+      { meta.map(({ format, key, style, editable }) => {
+        // I agree that content editable here is not the best choice, but suddenly, during dev,
+        // I liked how it looks and performs and how much code require
+        return editable
+          ? <td style={ { ...style, cursor: 'pencil', background: '#dadada' } } key={ key } contentEditable
+                onInput={ editNumberFromContentEditable(item) }>
+            { format(item[key]) }
+          </td>
+          : <td style={ style } key={ key }>{ format(item[key]) }</td>
+      }) }
+    </tr>) }
+    <tr>{ meta.map(({ total, format, key }) => {
+      return <th key={ key }>{ total && total !== 'avg' ? total : format(values
+        .reduce((sum, item) => {
+          return sum + (item.hasOwnProperty('tempTotal' + key) ? item['tempTotal' + key] : item[key])
+        }, 0) / (total === 'avg' ? values.length : 1)) }</th>
+    }) }</tr>
+  </Table>;
+};
